788e049e56a17d982a66dee74c5c295a3497186d
matdave <mat@matdave.com>
2025-08-22 13:16:51 -0500

    feat: strip EXIF data

--- core/components/formit/src/FormIt/Model/FormItForm.php
+++ core/components/formit/src/FormIt/Model/FormItForm.php
@@ -1,6 +1,7 @@
 <?php
 namespace Sterc\FormIt\Model;
 
+use Imagick;
 use xPDO\xPDO;
 use MODX\Revolution\modX;
 use MODX\Revolution\modSystemSetting;
@@ -259,6 +260,9 @@ class FormItForm extends \xPDO\Om\xPDOSimpleObject
             return;
         }
 
+        /* Strip Exif data */
+        $this->stripExif($tmp_name);
+
         /* Check filesize */
         $maxFileSize = $this->xpdo->getOption('upload_maxsize', null, 1048576);
         $size        = filesize($tmp_name);
@@ -422,6 +426,69 @@ class FormItForm extends \xPDO\Om\xPDOSimpleObject
                     }
                 }
                 rmdir($path);
+            }
+        }
+    }
+
+    private function stripExif($tmp_name): void
+    {
+        // check if the file is an image
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        if (false === $finfo) {
+            return;
+        }
+        $mime = finfo_file($finfo, $tmp_name);
+        finfo_close($finfo);
+        if (strpos($mime, 'image/') !== 0) {
+            return;
+        }
+        // try Imagick first
+
+        if (class_exists('Imagick')) {
+            try {
+                $image = new \Imagick($tmp_name);
+                $image->stripImage();
+                $image->writeImage($tmp_name);
+                $image->clear();
+            } catch (\Exception $e) {
+                $this->modx->log(\xPDO::LOG_LEVEL_ERROR, 'Imagick error: ' . $e->getMessage());
+            }
+        } elseif (function_exists('exif_imagetype')) {
+            $type = exif_imagetype($tmp_name);
+            $version = phpversion();
+            $image = null;
+            // try GD method
+            if (($type == IMAGETYPE_JPEG || $type == IMAGETYPE_JPEG2000) && function_exists('imagecreatefromjpeg')) {
+                $image = imagecreatefromjpeg($tmp_name);
+                if (!$image) return;
+                imagejpeg($image, $tmp_name, 100);
+            } elseif ($type == IMAGETYPE_PNG && function_exists('imagecreatefrompng')) {
+                $image = imagecreatefrompng($tmp_name);
+                if (!$image) return;
+                imagepng($image, $tmp_name, 9);
+            } elseif ($type == IMAGETYPE_GIF && function_exists('imagecreatefromgif')) {
+                $image = imagecreatefromgif($tmp_name);
+                if (!$image) return;
+                imagegif($image, $tmp_name);
+            } elseif ($type == IMAGETYPE_BMP && function_exists('imagecreatefrombmp')) {
+                $image = imagecreatefrombmp($tmp_name);
+                if (!$image) return;
+                imagebmp($image, $tmp_name);
+            } elseif ($type == IMAGETYPE_WEBP && function_exists('imagecreatefromwebp')) {
+                $image = imagecreatefromwebp($tmp_name);
+                if (!$image) return;
+                imagewebp($image, $tmp_name, 100);
+            } elseif (
+                version_compare($version, '8.1.0', 'ge')
+                && $type == 19
+                && function_exists('imagecreatefromavif')
+                && function_exists('imageavif')) {
+                $image = imagecreatefromavif($tmp_name);
+                if (!$image) return;
+                imageavif($image, $tmp_name, 100);
+            }
+            if ($image) {
+                imagedestroy($image);
             }
         }
     }
